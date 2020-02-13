b2b1fd53d95c82d515080c58ce37bffe968fa7ad
Nicholas Boleky <hotfeet100@hotmail.com>
2020-02-13 10:17:34 -0700

    Added one or two things

---
+++ Memory/Memory/Views/CardCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  CardCollectionViewCell.swift
+//  Memory
+//
+//  Created by Nicholas Boleky on 2/13/20.
+//  Copyright © 2020 Maxwell Poffenbarger. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cardFrontImage: UIImageView!
+    @IBOutlet weak var cardBackImage: UIImageView!
+    
+    var card: Card?
+    
+    func setCard(_ card: Card) {
+        self.card = card
+        
+        if card.isMatched == true {
+            cardBackImage.alpha = 0
+            cardFrontImage.alpha = 0
+            
+            return
+        } else {
+            cardBackImage.alpha = 1
+            cardFrontImage.alpha = 1
+        }
+        
+        cardFrontImage.image = UIImage(named: card.imageName)
+        
+        if card.isFlipped == true {
+            UIView.transition(from: cardBackImage, to: cardFrontImage, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        } else {
+            UIView.transition(from: cardFrontImage, to: cardBackImage, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
+    }
+    
+    func flipCardFront() {
+        UIView.transition(from: cardBackImage, to: cardFrontImage, duration: 0.5, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+    }
+    
+    func flipCardBack() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+            UIView.transition(from: self.cardFrontImage, to: self.cardBackImage, duration: 0.5, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+        }
+    }
+    
+    func removeMatchedCards() {
+        cardBackImage.alpha = 0
+
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: .curveEaseOut, animations: {
+            self.cardFrontImage.alpha = 0
+        }, completion: nil)
+    }
+    
+    
+}
